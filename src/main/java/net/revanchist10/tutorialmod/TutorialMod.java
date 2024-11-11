9cc64d0f74d5cec840fa220a7095721281a81566
Revanchist10 <jacksonheadrick71@gmail.com>
2024-11-11 01:10:10 -0600

    learning how to commit

---
+++ src/main/java/net/revanchist10/tutorialmod/TutorialMod.java
@@ -0,0 +1,18 @@
+package net.revanchist10.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorial-mod";
+//very important
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
